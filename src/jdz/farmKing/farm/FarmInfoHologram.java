a17b2771aa6effc4c62bb04342975a5526377d01
bakerjaid <jaidencolebaker@gmail.com>
2019-02-13 16:17:49 +1300

    Extracted FarmInfoHologram from Farm

---
+++ src/jdz/farmKing/farm/FarmInfoHologram.java
@@ -0,0 +1,47 @@
+
+package jdz.farmKing.farm;
+
+import static jdz.UEconomy.UEcoFormatter.charFormat;
+import static jdz.UEconomy.UEcoFormatter.makeWhole;
+
+import static org.bukkit.ChatColor.*;
+import org.bukkit.Location;
+
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.line.TextLine;
+
+import jdz.farmKing.HologramManager;
+
+public class FarmInfoHologram {
+	private Farm farm;
+	private Hologram hologram;
+	private TextLine gemMultiplierLine, gemsFromResetLine;
+
+	public FarmInfoHologram(Farm farm) {
+		this.farm = farm;
+
+		hologram = HologramManager.make(farm.getOwner(), getLocation());
+		hologram.appendTextLine(YELLOW + "FARM DETAILS");
+		hologram.appendTextLine("");
+		gemMultiplierLine = hologram.appendTextLine("");
+		gemsFromResetLine = hologram.appendTextLine("");
+		hologram.appendTextLine("");
+		hologram.appendTextLine(GREEN + "Bonuses from farm level " + farm.getLevel() + ":");
+		hologram.appendTextLine(RED + "None");
+		hologram.appendTextLine("");
+		hologram.appendTextLine(
+				GREEN + "Gems required for level up: " + makeWhole(charFormat(farm.getGemReq(farm.getLevel() + 1), 4)));
+	}
+
+	public void update() {
+		gemMultiplierLine.setText(
+				GREEN + "Multiplier from gems: " + YELLOW + "x" + charFormat(farm.getGemIncomeMultiplier(), 4));
+		gemsFromResetLine.setText(GREEN + "Gems gained from resetting: " + YELLOW
+				+ makeWhole(charFormat(farm.getGemsFromResetting(), 4)));
+	}
+
+	public Location getLocation() {
+		return farm.spawn.clone().add(0, 3, 3);
+	}
+
+}
